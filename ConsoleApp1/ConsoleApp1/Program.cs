2736d6a282c9aaf502671c3e8bf1708fe0dd4a68
Jaysson Andres Avendaño Barbosa <javendanob@intergrupo.com>
2018-05-19 22:22:32 +0000

    ..

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 namespace ConsoleApp1
 {
     class Program
     {
+        Newtonsoft.Json.JsonSerializer _serializer = new JsonSerializer();
         static void Main(string[] args)
         {
             Console.Write("Hola");
